7a837cb653c340e22a0ce2581530fae41f07df89
Matt <GlutenFreeCoder@users.noreply.github.com>
2017-08-02 09:58:19 -0400

    Create SendWOL.ps1

---
+++ SendWOL.ps1
@@ -0,0 +1,31 @@
+<# 
+  .SYNOPSIS  
+    Send a WOL packet to a broadcast address  Usage: SendWOL -mac [address]
+
+  .PARAMETER mac
+   The MAC address of the device that need to wake up
+
+  .EXAMPLE
+  .\SendWOL -mac 74:D0:2B:7C:8E:B3
+#>
+
+[CmdletBinding()]
+Param(
+	[Parameter(Mandatory=$True,Position=1)]
+	[string]$mac
+	)
+
+## Construct the Magic Packet
+$MacByteArray = $mac -split "[:-]" | ForEach-Object { [Byte] "0x$_"}
+[Byte[]] $MagicPacket = (,0xFF * 6) + ($MacByteArray  * 16)
+
+## Create UDP client instance
+$UdpClient = New-Object System.Net.Sockets.UdpClient
+$UdpClient.Connect(([System.Net.IPAddress]::Broadcast),7) 
+
+## Broadcast the Packet
+$UdpClient.Send($MagicPacket,$MagicPacket.Length)
+Write-Host "Magic Packet sent to: $mac"
+
+## Close the resource
+$UdpClient.Close()
